f149351078cd2e5a53b30d1763b778808183eb16
Mike Wiedemann <m.wiedemann@8man.com>
2017-07-26 22:32:08 +0200

    Add structure to branch

---
+++ src/app/recipes/recipe-list/receipe-item/receipe-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-item',
+  templateUrl: './receipe-item.component.html',
+  styleUrls: ['./receipe-item.component.css']
+})
+export class RecipeItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
