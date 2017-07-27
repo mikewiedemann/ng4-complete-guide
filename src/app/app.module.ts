59657b641ed68484d3cfd5dadf93473d8a86eab3
Mike Wiedemann <m.wiedemann@8man.com>
2017-07-27 08:25:13 +0200

    Chapter 43 part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipes/recipe-list/receipe-item/receipe-item.component';
-import { ReceipeDetailComponent } from './recipes/receipe-detail/receipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
@@ -17,7 +17,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     RecipesComponent,
     RecipeListComponent,
     RecipeItemComponent,
-    ReceipeDetailComponent,
+    RecipeDetailComponent,
     ShoppingListComponent,
     ShoppingEditComponent
   ],
--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-item',
-  templateUrl: './receipe-item.component.html',
-  styleUrls: ['./receipe-item.component.css']
+  templateUrl: './recipe-item.component.html',
+  styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
 
